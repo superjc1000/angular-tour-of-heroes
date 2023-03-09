532920c04cb63c69bdeceb4b91356bc251a16259
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-03-09 12:42:32 +0100

    Ejercicio 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Peñetazo' },
+  { id: 13, name: 'Bofetacus' },
+  { id: 14, name: 'Capitan calatayud' },
+  { id: 15, name: 'Alfonso el espabilao' },
+  { id: 16, name: 'Cassaieta woman' },
+  { id: 17, name: 'Er Toro' },
+  { id: 18, name: 'Mesie fganses' },
+  { id: 19, name: 'Asporsaret girl' },
+  { id: 20, name: 'El Aberroncho' }
+];
